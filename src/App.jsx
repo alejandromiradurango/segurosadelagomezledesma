2a189d41a010b7654a107d3b100d4b45e0b16232
Alejandro Mira Durango <89473354+alejandromiradurango@users.noreply.github.com>
2023-10-05 14:55:56 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import { Blog, Contact, Home, Layout, Projects } from './pages'
+import { useLayoutEffect } from 'react'
+
+const Wrapper = ({ children }) => {
+  const location = useLocation()
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0)
+  }, [location.pathname])
+  return children
+}
+
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Home />}/>
-          <Route path='proyectos' element={<Projects />}/>
-          <Route path='blog' element={<Blog />}/>
-          <Route path='contacto' element={<Contact />}/>
-        </Route>
-      </Routes>
+      <Wrapper>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />}/>
+            <Route path='proyectos' element={<Projects />}/>
+            <Route path='blog' element={<Blog />}/>
+            <Route path='contacto' element={<Contact />}/>
+          </Route>
+        </Routes>
+      </Wrapper>
     </Router>
   )
 }
